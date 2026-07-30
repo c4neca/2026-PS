2471b791f5bbc38e1af93413a8012199510e96a6
c4neca <20251ctb0100028@estudantes.ifpr.edu.br>
2026-07-30 15:12:37 +0000

    adicao de atributos privados e construtores

--- 03_java/aula30/Chamado.java
+++ 03_java/aula30/Chamado.java
@@ -2,4 +2,20 @@ public class Chamado {
     private String idChamado;
     private String titulo;
     private String descricao;
+    private int prioridade;
+    private String status;
+    private int diasAberto;
+
+    public Chamado(String idChamado, String titulo, String descricao, int prioridade) {
+        this.idChamado = idChamado;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.status = "ABERTO";
+        this.diasAberto = 0;
+    }
+
+    public Chamado(String idChamado, String titulo, String descricao) {
+        this(idChamado, titulo, descricao, 1);
+    }
 }
